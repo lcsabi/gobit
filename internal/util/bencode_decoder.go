6681afcab6ba4e48394be577316cf9066c31d2b8
László Csaba <laszlo.cs4ba@gmail.com>
2025-06-16 18:50:45 +0200

    implement integer and byte string parsing

---
+++ internal/util/bencode_decoder.go
@@ -0,0 +1,86 @@
+// https://wiki.theory.org/BitTorrentSpecification#Bencoding
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+type BencodedValue any
+
+func parseBencode(r *bytes.Reader) (BencodedValue, error) {
+	// read delimiter
+	delimiter, err := r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	switch {
+	case delimiter == 'i':
+		return parseInt(r)
+	case delimiter >= '0' && delimiter <= '9':
+		// delimiter is also the first digit of the Byte String's length
+		return parseByteString(r, delimiter)
+	default:
+		return nil, fmt.Errorf("invalid bencode prefix: %c", delimiter)
+	}
+}
+
+func parseInt(r *bytes.Reader) (int64, error) {
+	var buffer bytes.Buffer
+	for {
+		digit, err := r.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+
+		if digit == 'e' {
+			break
+		}
+		buffer.WriteByte(digit)
+	}
+
+	return strconv.ParseInt(buffer.String(), 10, 64)
+}
+
+func parseByteString(r *bytes.Reader, firstDigit byte) (string, error) {
+	var buffer bytes.Buffer
+	buffer.WriteByte(firstDigit)
+
+	for {
+		digit, err := r.ReadByte()
+		if err != nil {
+			return "", err
+		}
+
+		if digit == ':' {
+			break
+		}
+		buffer.WriteByte(digit)
+	}
+	byteStringLength, err := strconv.ParseInt(buffer.String(), 10, 64)
+	if err != nil {
+		return "", err
+	}
+
+	byteString := make([]byte, byteStringLength)
+	_, err = io.ReadFull(r, byteString)
+	if err != nil {
+		return "", err
+	}
+
+	return string(byteString), nil
+}
+
+func main() {
+	input := []byte("4:spam")
+	reader := bytes.NewReader(input)
+	val, err := parseBencode(reader)
+	if err != nil {
+		fmt.Printf("error %v\n", err)
+	}
+	fmt.Println(val)
+}
